3bb6b4636ae6bcd216a09692d0d9fc69f394d984
csdTeam <kleer@kleer-csd-box>
2018-04-20 10:05:42 -0500

    Implementando tablero de juego

--- app.rb
+++ app.rb
@@ -2,5 +2,9 @@ require 'sinatra'
 require './config'
 
 get '/' do
+erb :index
+end
 
+post '/iniciarJuego' do 
+'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
 end
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,7 +1,13 @@
-Given(/^Que abri el juego$/) do
+Given(/^que abri el juego$/) do
     visit '/'
 end
 
 Then(/^debo ver "([^"]*)"$/) do |texto|
   expect(page.body).to match /#{texto}/m
+end
+
+
+Given(/^inicie un nuevo juego$/) do
+  visit '/'
+  click_button('Iniciar')
 end
