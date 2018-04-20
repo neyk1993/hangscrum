a0ff2e9ac52a28e81662db40ac6c44c24e257a3a
equipo 1 <you@example.com>
2018-04-20 11:59:06 -0500

    Intregacion del modelo

---
+++ spec/juego_spec.rb
@@ -0,0 +1,13 @@
+require './lib/juego.rb'
+describe Juego do 
+	it "jugardor escribe a y la entra muestra _a_a" do 
+		juego=Juego.new 
+		result = juego.adivina "a"
+		expect(result).to eq "_a_a"
+	end
+end
+
+
+#"jugardor escribe c y la entra muestra ca_a"
+#"jugador escribe x y no la encuentra en casa muestra ca_a"
+#"jugador escibre s y la encuentra muestra muestra casa"
