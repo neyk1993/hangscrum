85f95ce8f452f49ddebfab752a42a616963e4fe7
equipo 1 <you@example.com>
2018-04-20 14:33:33 -0500

    Integracion del modelo y tdd

--- lib/juego.rb
+++ lib/juego.rb
@@ -1,22 +1,35 @@
 class Juego
 
 	def initialize 
+		@letras = {"A" =>"","B"=>"","C"=>"","D"=>"","E"=>"","F"=>"","G"=>"","H"=>"","I"=>"","J"=>"","K"=>"","L"=>"","M"=>"","N"=>"","Ñ"=>"","O"=>"","P"=>"","Q"=>"","R"=>"","S"=>"","T"=>"","U"=>"","V"=>"","W"=>"","X"=>"","Y"=>"","Z"=>""}
 		@palabra = ["S","C","R","U","M"]
 		@palabra_adivinada = ["-","-","-","-","-"]
 	end
 	def adivina letra
-		if @palabra.to_s.include? letra
-			posicion = 0
-			@palabra.each do |item|
-				if item.eql? letra
-					@palabra_adivinada[posicion] = letra					
-				end
-				posicion += 1
-			end
 
-			return @palabra_adivinada.join ""
-		else
-			@palabra_adivinada.join ""
-		end
+			posicionletra = 0
+			@letras.each do |letrapermitida,estadoletra|
+				if letrapermitida.eql? letra
+					@letras[letra] = "S"
+				end
+				posicionletra +=1
+			end
+			if @palabra.to_s.include? letra
+				posicion = 0
+				@palabra.each do |item|
+					if item.eql? letra
+						@palabra_adivinada[posicion] = letra					
+					end
+					posicion += 1
+				end
+
+				return @palabra_adivinada.join ""
+			else
+				@palabra_adivinada.join ""
+			end
+	end
+
+	def obtenerletras 
+		return @letras
 	end
 end
